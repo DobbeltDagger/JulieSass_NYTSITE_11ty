eedfebdb04ca67608c5e7d1e5f17d4576b820d2e
Anders Visti <mail@andersvisti.dk>
2023-10-18 23:21:11 +0200

    more design

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -18,36 +18,7 @@ const resize = () => {
 }
 
 
-////////////////////////////////////
-// client side readable time function
-const getTime = () => {
-
-  var date = new Date();
-  let year = date.getFullYear();
-  let shortYear = year.toString().substr(-2);
-  let month = date.getMonth()+1;
-  let day = date.getDate();
-  let hour = date.getHours();
-  let min = date.getMinutes();
-  let sec = date.getSeconds();
-
-  month = (month < 10 ? "0" : "") + month;
-  day = (day < 10 ? "0" : "") + day;
-  hour = (hour < 10 ? "0" : "") + hour;
-  min = (min < 10 ? "0" : "") + min;
-  sec = (sec < 10 ? "0" : "") + sec;
-
-  let longTime = day + '/' + month + '/' + year + ' - ' + hour + ':' + min;
-  let shortTime = day + '/' + month + '/' + shortYear + '-' + hour + ':' + min + ':' + sec;
-  // let shortTime = day + '/' + month + '/' + shortYear + '-' + hour + ':' + min;
-
-  return {
-    long: longTime,
-    short: shortTime
-  }
-}
-
-
+///////////////////////////////////
 export {
-  resize, getTime
+  resize
 }
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,19 @@
 import { resize } from './functions.js';
-import { App } from './App.js';
 
-
+//////////////////////////////////////////////////////
+// DOM loaded
 document.addEventListener("DOMContentLoaded", (e) => {
 
-  let app = new App();
-  app.init();
+  // get the menu burger
+  let burgerBtn = document.getElementById('burger');
+  burgerBtn.addEventListener('click', (e)=> {
+    console.log("burger was clicked!")
+    e.preventDefault();
+
+    document.getElementById('mobileNavWrapper').classList.toggle("hide");
+    document.getElementById('burgerIcon').classList.toggle('hide');
+    document.getElementById('crossIcon').classList.toggle('hide');
+  });
 
   window.onresize = resize;
   resize();
